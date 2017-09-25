bb0d52bc2d18768d2c2350abc5f15c44225d7992
Winter LaMon <winter.lamon@gmail.com>
2017-09-25 01:37:01 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,10 +55,10 @@ end
 
 def runner
   welcome
-#  card_total = initial_round
-#  until card_total > 21
-#    hit?(card_total)
-#    display_card_total(card_total)
-#  end
-#  end_game(card_total)
+  card_total = initial_round
+  until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
