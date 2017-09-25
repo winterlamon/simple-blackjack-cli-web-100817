6b2c5c0971cba6f6184b69e8039acbc47e89dd68
Winter LaMon <winter.lamon@gmail.com>
2017-09-25 01:43:16 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,9 +49,7 @@ def invalid_command
   puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+# --- #RUNNER (gameplay method) --- #
 
 def runner
   welcome
